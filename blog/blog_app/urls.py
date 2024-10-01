f24a50a11d0a1a8fddec7cf1e10f878f3385a7a6
Jaspreet Singh <140846697+jaspreet-si@users.noreply.github.com>
2024-10-01 10:34:31 +0530

    Update urls.py

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -6,9 +6,8 @@ from . import views
 urlpatterns = [
     path('login/',views.login,name='login'),
     path('signup/',views.Signup,name='signup'),
-    
     path('home/',views.blogs_listing,name='home'),
     path('blogs/<int:id>/',views.blog_detail,name='blog_detail'),
     path('comment/like/<int:comment_id>/',views.like_comment, name='like_comment'),
-     path('blog/share/<int:blog_id>/',views.share_blog, name='share_blog'),
+    path('blog/share/<int:blog_id>/',views.share_blog, name='share_blog'),
 ]
